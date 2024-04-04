b0d63d2eed47870e27ba1d6490c92d061b89b200
budnys <diego@budnys.com>
2024-04-04 12:44:07 -0400

    nlw_aula1: config inicial

---
+++ base.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS "events" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "title" TEXT NOT NULL,
+    "details" TEXT,
+    "slug" TEXT NOT NULL,
+    "maximum_attendees" INTEGER
+);
+
+CREATE TABLE IF NOT EXISTS "attendees" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "event_id" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY (event_id) REFERENCES "events" (id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS "check_ins" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "attendee_id" TEXT NOT NULL,
+    CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY (attendee_id) REFERENCES "attendees" (id) ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+CREATE UNIQUE INDEX "events_slug_key" ON "events"(slug);
+CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"(event_id, email);
+CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"(attendee_id);
