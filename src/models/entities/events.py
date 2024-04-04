0f995f1feff7749f0aa7a90bbda27575b19d4e1e
budnys <diego@budnys.com>
2024-04-04 18:19:10 -0400

    nlw_aula1: end of day 1

--- src/models/entities/events.py
+++ src/models/entities/events.py
@@ -1,11 +1,14 @@
-from scr.models.settings.base import Base
+from src.models.settings.base import Base
 from sqlalchemy import Column, String, Integer
 
 class Events(Base):
     __tablename__ = "events"
 
-    id = Columnn(String, primary_key=True)
+    id = Column(String, primary_key=True)
     title = Column(String, nullable=False)
     details = Column(String)
     slug = Column(String, nullable=False)
     maximum_attendees = Column(Integer)
+
+    def __repr__(self):
+        return f"Events object with data: [title={self.title}, masimum_attendees={self.maximum_attendees}]"
---
+++ src/models/repository/events_repository_test.py
@@ -0,0 +1,23 @@
+import pytest
+from src.models.settings.connection import db_connection_handler
+from .events_repository import EventsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Novo registro em banco de dados")
+def test_insert_event():
+    event = {
+        "uuid": "meu-uuid",
+        "title": "meu title",
+        "slug": "meu-alug",
+        "maximum_attendees": 50
+    }
+    events_repository = EventsRepository()
+    response = events_repository.insert_event(event)
+    print(response)
+
+def test_get_event_by_id():
+    event_id = "meu-uuid"
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id)
+    print(response)
